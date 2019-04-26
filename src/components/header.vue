1cd5ff33879806b48e5f1f8705fc8073eeb6cb81
minatoyukina <1096445518@qq.com>
2019-04-26 12:26:25 +0800

    setup

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="main-header" :style="{backgroundImage:`url(${img})`}">
+    <div class="main-header-box">
+      <a class="header-avatar" href="/">
+        <img src="../assets/img/avatar.jpg" class="img-responsive center-block"/>
+      </a>
+      <div class="branding">
+        <img src="../assets/img/branding.png" class="img-responsive center-block"/>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "header",
+    data() {
+      return {
+        img: require('./../assets/img/head-img.jpg')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /*@import "../assets/css/bootstrap.min.css";*/
+  @import "../assets/css/font-awesome.min.css";
+  @import "../assets/css/style.css";
+</style>
